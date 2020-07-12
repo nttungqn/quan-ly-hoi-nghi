5a39433b8672887657c8ec6975a4d7a0750e6c16
nttung <1712882@student.hcmus.edu.vn>
2020-07-12 22:25:28 +0700

    + gui: login + dashboard

---
+++ src/main/java/Controllers/Login.java
@@ -0,0 +1,11 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+
+public class Login {
+    public void handleLoginButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void handleCancelButtonAction(ActionEvent actionEvent) {
+    }
+}
